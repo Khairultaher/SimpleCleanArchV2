cb4dfc83a1fa12fbf32a989a5c6bea8a1b638df4
khairul alam <khairul.alam@vivacomsolutions.com>
2022-10-19 15:40:33 +0600

    predicate builder added

--- src/Services/WeatherForecast/WeatherForecast.Application/Features/WeatherForecast/Queries/GetWeatherForecast/GetWeatherForecastWithPaginationQuery.cs
+++ src/Services/WeatherForecast/WeatherForecast.Application/Features/WeatherForecast/Queries/GetWeatherForecast/GetWeatherForecastWithPaginationQuery.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
+using System.Linq;
 using WeatherForecast.Application.Common.Mappings;
+using WeatherForecast.Application.Extensions;
 using WeatherForecast.Application.Interfaces.Persistence;
 using WeatherForecast.Application.Models;
 using WeatherForecast.Application.WeatherForecasts.Queries.GetWeatherForecast;
+using WeatherForecast.Domain.Entities;
 
 namespace WeatherForecast.Application.Features.WeatherForecast.Queries.GetWeatherForecast
 {
@@ -15,6 +18,8 @@ namespace WeatherForecast.Application.Features.WeatherForecast.Queries.GetWeathe
         //public int ListId { get; set; }
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+
+        public string? Location { get; set; }
     }
 
     public class GetWeatherForecastWithPaginationQueryHandler
@@ -30,10 +35,17 @@ namespace WeatherForecast.Application.Features.WeatherForecast.Queries.GetWeathe
         public async Task<PaginatedList<WeatherForecastModel>> Handle(GetWeatherForecastWithPaginationQuery request, CancellationToken cancellationToken)
         {
 
+            var predicate = PredicateBuilder.True<WeatherForecastEntity>();
+            if (!string.IsNullOrEmpty(request.Location))
+            {
+                predicate = predicate.And(i => i.Location!.ToLower() == request.Location.ToLower());
+            }
+    
             return await _context.WeatherForecasts
-            .OrderByDescending(x => x.Id)
-            .ProjectTo<WeatherForecastModel>(_mapper.ConfigurationProvider)
-            .PaginatedListAsync(request.PageNumber, request.PageSize);
+                .Where(predicate)
+                .OrderByDescending(x => x.Id)
+                .ProjectTo<WeatherForecastModel>(_mapper.ConfigurationProvider)
+                .PaginatedListAsync(request.PageNumber, request.PageSize);
         }
     }
 }
